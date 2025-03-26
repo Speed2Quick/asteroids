b40301caa7ff45f747b05105ebeea9da1b577f58
Speed2Quick <4samuelr05@gmail.com>
2025-03-25 20:56:28 -0400

    create groups and player movement

--- main.py
+++ main.py
@@ -10,15 +10,25 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     clock = pygame.time.Clock()
     dt = 0
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
-    while 1 > 0:
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+        
+        updatable.update(dt)
+         
         screen.fill(0)
-        player.draw(screen)
+        
+        for sprite in drawable:
+            sprite.draw(screen)
+
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
