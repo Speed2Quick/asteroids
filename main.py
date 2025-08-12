6b7b67c909fcb6a30da79f9914641e601a2f549e
Speed2Quick <4samuelr05@gmail.com>
2025-08-12 19:02:06 -0400

    create player and add movement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print("Starting Asteroids!")
@@ -8,15 +9,33 @@ def main():
 
     pygame.init()
 
+    Clock = pygame.time.Clock()
+    dt = 0
+
+    drawable = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
+
+    Player.containers = (drawable, updatable)
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        updatable.update(dt)
+
         screen.fill("black")
+        
+        for sprite in drawable:
+            sprite.draw(screen)
+
         pygame.display.flip()
+
+        dt = Clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
